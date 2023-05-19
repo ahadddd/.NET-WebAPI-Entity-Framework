f460464a6d5fbb49ec9b246aa91bdef9b3f3f1d9
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-19 15:14:10 +0500

    Add project files.

---
+++ PokemonApp/Models/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonApp.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthdate { get; set; }   
+    }
+}
