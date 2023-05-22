086ba3391fd7d871968aaa92aca3997e5c2c8d98
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-22 15:38:30 +0500

    First request configured

---
+++ PokemonApp/Repositories/PokemonRepository.cs
@@ -0,0 +1,21 @@
+﻿using PokemonApp.Data;
+using PokemonApp.Interfaces;
+using PokemonApp.Models;
+
+namespace PokemonApp.Repositories
+{
+    public class PokemonRepository: IPokemonRepository
+    {
+        private readonly DataContext _context;
+
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
