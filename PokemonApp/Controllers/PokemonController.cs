22b20558be1c5f063a24f55f49106e891d97a71b
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-22 16:56:31 +0500

    New routes defined

--- PokemonApp/Controllers/PokemonController.cs
+++ PokemonApp/Controllers/PokemonController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using PokemonApp.Interfaces;
 using PokemonApp.Models;
 
@@ -18,7 +19,6 @@ namespace PokemonApp.Controllers
 
         [HttpGet]
         [ProducesResponseType(200, Type =typeof(ICollection<Pokemon>))]
-
         public IActionResult GetPokemons()
         {
             var pokemons = _pokemonRepository.GetPokemons();
@@ -32,6 +32,39 @@ namespace PokemonApp.Controllers
                 return Ok(pokemons);
             }
         }
+
+        [HttpGet("{pokeID}")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int pokeID)
+        {
+            if(_pokemonRepository.PokemonExists(pokeID) == false)
+                return NotFound();
+                
+            var pokemon = _pokemonRepository.GetPokemon(pokeID);
+            
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(pokemon);
+        }
+
+
+        [HttpGet("{pokeID}/rating")]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonRating(int pokeID)
+        {
+            if (_pokemonRepository.PokemonExists(pokeID) == false)
+                return NotFound();
+
+            var rating = _pokemonRepository.GetPokemonRating(pokeID);
+            if (!ModelState.IsValid)
+                return BadRequest();
+            return Ok(rating);
+        }
+
+
+
 
     }
 }
