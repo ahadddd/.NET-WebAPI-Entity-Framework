f23de148113c09ab0bd44b2c4dd4b5b06b7c5487
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-23 16:05:03 +0500

    Controller and Repository configured for Reviewer

---
+++ PokemonApp/Interfaces/IReviewerRepository.cs
@@ -0,0 +1,15 @@
+﻿using PokemonApp.Models;
+
+namespace PokemonApp.Interfaces
+{
+    public interface IReviewerRepository
+    {
+        Reviewer GetReviewer(int reviewerId);
+
+        ICollection<Reviewer> GetReviewers();
+
+        ICollection<Review> GetReviewsByReviewer(int reviewerId);
+
+        bool ReviewerExists(int reviewerId);
+    }
+}
---
+++ PokemonApp/Repositories/ReviewerRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonApp.Data;
+using PokemonApp.Interfaces;
+using PokemonApp.Models;
+
+namespace PokemonApp.Repositories
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private readonly DataContext _context;
+
+        public ReviewerRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Reviewer GetReviewer(int reviewerId)
+        {
+            return _context.Reviewers.Where(r => r.Id == reviewerId).FirstOrDefault();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _context.Reviewers.Include(e => e.Reviews).ToList();
+        }
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+        {
+            return _context.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public bool ReviewerExists(int reviewerId)
+        {
+            return _context.Reviewers.Any(r => r.Id == reviewerId);
+        }
+    }
+}
